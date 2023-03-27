594fe192387ec5f485eebf6b299114fa2d63f3a2
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2023-03-26 22:02:16 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WPF_Projects
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuBar_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
